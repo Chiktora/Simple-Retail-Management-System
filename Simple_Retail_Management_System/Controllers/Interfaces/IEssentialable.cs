417e0e4827e47970809193f069674cfd29600513
Chiktora <125090550+Chiktora@users.noreply.github.com>
2024-03-16 23:42:30 +0200

    Added IEssentialable interface. Implemented interface in ProductController and added logic.

---
+++ Simple_Retail_Management_System/Controllers/Interfaces/IEssentialable.cs
@@ -0,0 +1,29 @@
+﻿using Simple_Retail_Management_System.Data.Models;
+using Simple_Retail_Management_System.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Retail_Management_System.Controllers.Interfaces
+{
+    internal interface IEssentialable<T>
+    {
+        // Get all records
+        public List<T> GetAll();
+        
+        // Get record by id
+        public T Get(int id);
+
+        // Add item in table
+        public void Add(T item);
+
+        // Update item in table
+        public void Upgrade(T item);
+
+        // Delete item by id
+        public void Delete(int id);
+
+    }
+}
