6744a6cb96b0e36e2d6bb2abf26ebbbcee6e52f9
Drago <123863368+DragoSexa@users.noreply.github.com>
2024-03-22 12:13:25 +0200

    Update ProductDisplay.cs

--- Simple_Retail_Management_System/Views/ProductDisplay.cs
+++ Simple_Retail_Management_System/Views/ProductDisplay.cs
@@ -1,6 +1,55 @@
 ﻿namespace Simple_Retail_Management_System.Views
 {
-    internal class ProductDisplay
+public class ProductDisplay
+{
+    ProductController productController;
+
+    public ProductDisplay()
     {
+        productController = new ProductController();
+        ProductInput();
+    }
+
+    public void ProductMenu()
+    {
+        Console.WriteLine(new string('-', 14) + "ProductsMenu" + new string('-', 14));
+        Console.WriteLine("1. Add a new Product");
+        Console.WriteLine("2. Delete a Product");
+        Console.WriteLine("3. Find Product by Id");
+        Console.WriteLine("4. Show all Products");
+        Console.WriteLine("5. Update a Product");
+        Console.WriteLine(new string('-', 40));
+    }
+
+
+    private void ProductInput()
+    {
+        ProductMenu();
+
+        int operation = int.Parse(Console.ReadLine());
+        switch (operation)
+        {
+            case 1:
+                AddProduct();
+                Console.WriteLine("Product added.");
+                break;
+            case 2:
+                DeleteProduct();
+                break;
+            case 3:
+                ShowProduct();
+                break;
+            case 4:
+                ShowAllProducts();
+                break;
+            case 5:
+                UpdateProduct();
+                break;
+            default:
+                Console.WriteLine("Option not available!\nReturning to Main Menu.");
+                break;
+        }
+
+    }
     }
 }
